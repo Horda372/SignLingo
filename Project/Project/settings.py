9fbae3d1c214799d6be0faea73490379ea7f97b5
Nina <nowaknina648@gmail.com>
2025-06-16 22:04:57 +0200

    Connection to db

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -36,6 +36,7 @@ load_dotenv()
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
